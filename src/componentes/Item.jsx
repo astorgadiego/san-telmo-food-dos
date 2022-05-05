a931fa5dc775d779b96799622e14d698935a6d68
Diego Astorga <diego.astorga98@gmail.com>
2022-05-05 00:11:58 -0300

    push

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -3,7 +3,7 @@ function Item({art}) {
     <div key={art.id} style={{border:'solid green 5px', margin:'10px', padding:'5px'}}>
           <div>{art.titulo}</div>
           <div>${art.precio}</div>
-          <img className="visible m-4" src={art.imagenURL} alt=""></img>
+          <img src={art.imagenURL} alt=""></img>
     </div>
   )
 }

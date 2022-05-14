eedaf8a27c0db2c1ce00816f226091a9d166519d
Diego Astorga <diego.astorga98@gmail.com>
2022-05-14 18:11:47 -0300

    Limpiar Carrito

--- src/Store/CartContext.jsx
+++ src/Store/CartContext.jsx
@@ -10,54 +10,59 @@ const { Provider } = CartContext
 export function CartContextProvider({children}) {
     const [carrito, setcarrito] = useState([])
 
-    const AgregarAlCarrito = ( item, cant)=>{
-        /*const newCarrito= [...carrito];
-        setcarrito( newCarrito. concat (newItem))*/
-        if ( isInCarrito()){  
-            const newCart =  carrito.map(itemdecarrito =>{ if(itemdecarrito.id === item.id){
-                const copyItem = {...itemdecarrito}
-                copyItem.cant += cant
-                return copyItem
-            }
-                else
-                    return itemdecarrito
-            } ) 
-            setcarrito(newCart)
+        const AgregarAlCarrito = ( item, cant)=>{
+                /*const newCarrito= [...carrito];
+                setcarrito( newCarrito. concat (newItem))*/
+                if ( isInCarrito()){  
+                    const newCart =  carrito.map(itemdecarrito =>{ if(itemdecarrito.id === item.id){
+                        const copyItem = {...itemdecarrito}
+                        copyItem.cant += cant
+                        return copyItem
+                    }
+                        else
+                            return itemdecarrito
+                    } ) 
+                    setcarrito(newCart)
+                }
+                else{
+                    const newItem = { ...item, cant}
+                    setcarrito( [...carrito, newItem] )
+                }
         }
-        else{
-            const newItem = { ...item, cant}
-            setcarrito( [...carrito, newItem] )
+
+        const removeFromCart = (id)=>{
+                const newCarrito = [ ...carrito]
+                const filtrarcarrito = newCarrito.filter( item =>{
+                    return item.id !== id
+                })
+                setcarrito(filtrarcarrito)
         }
-    }
 
-    const removeFromCart = (id)=>{
-        const newCarrito = [ ...carrito]
-        const filtrarcarrito = newCarrito.filter( item =>{
-            return item.id !== id
-        })
-        setcarrito(filtrarcarrito)
-    }
+        const ClearCarrito =()=>{
+            setcarrito([])
+        }
 
-    const isInCarrito = ()=>{
-        return true;
-    }
+        //POR AHORA ESTA EN MODO MANUAL. 
+        const isInCarrito = ()=>{
+            return false;
+        }
  
-    const Funcioncontexto = ()=>console.log("Contexto Listo!!")    
+        const Funcioncontexto = ()=>console.log("Contexto Listo!!")    
 
-    //NAVBAR
-    const CantidadenCarrito = ()=>{
-        const cantidad = 0
-        return cantidad
-    }
+        //NAVBAR
+        const CantidadenCarrito = ()=>{
+            const cantidad = 0
+            return cantidad
+        }
 
-    //CART
-    const TotalCompra = ()=>{
-        const preciototal = 0
-        return preciototal
-    }
+        //CART
+        const TotalCompra = ()=>{
+            const preciototal = 0
+            return preciototal
+        }
 
     return(
-        <Provider value={ { Funcioncontexto, carrito, AgregarAlCarrito, removeFromCart } }>
+        <Provider value={ { Funcioncontexto, carrito, AgregarAlCarrito, removeFromCart, ClearCarrito } }>
             {children}
         </Provider>
     )
